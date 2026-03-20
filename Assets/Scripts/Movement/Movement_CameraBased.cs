ef0ca209192d52090d0a012425b808ca73d2a311
MHermosillo <hermosillo.quezada@gmail.com>
2026-03-20 13:43:09 -0600

    Attempted to Import Models
    Attempted to import models from animation department. However, they have scale and transformation issues and some have no baked animation components when imported to Unity despite insinuating so.
    
    Also added documentation for movement script which was found missing. Alongside adding model rotation relative to movement direction

--- Assets/Scripts/Movement/Movement_CameraBased.cs
+++ Assets/Scripts/Movement/Movement_CameraBased.cs
@@ -3,30 +3,43 @@ using UnityEngine.InputSystem;
 
 public class Movement_CameraBased : MonoBehaviour
 {
+    //movement speed
     [SerializeField] private float speed = 3;
+    //jump height multiplier
     [SerializeField] private float jumpHeight = 2;
+    //gravity multiplier
     [SerializeField] private float gravity = -9.8f;
 
+    //reference to movement controller
     private CharacterController controller;
+
+    //stores move input, output, and current velocity
     Vector2 moveInput;
     Vector3 moveOutput;
     Vector3 velocity;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Gets reference to movement controller
         controller = GetComponent<CharacterController>();
     }
     
+    //When method is called after pressing the button associated
+    //with the method in the input controller component of player
     public void OnMove(InputAction.CallbackContext context) 
     {
+        //Get the movement input
         moveInput = context.ReadValue<Vector2>();
 
-        //move inputs
+        //Store the horizontal and vertical inputs separately
+        //(forward/right inputs)
         float horizontalInput = moveInput.x;
         float verticalInput = moveInput.y;
 
-        //Camera direction vectors
+        //Get the camera direction vectors
+        //and clean the data to only leave
+        //respective horizontal and vertical vectors
+        //(forward/right vectors)
         Vector3 forward = Camera.main.transform.forward;
         Vector3 right = Camera.main.transform.right;
         forward.y = 0;
@@ -34,26 +47,43 @@ public class Movement_CameraBased : MonoBehaviour
         forward = forward.normalized;
         right = right.normalized;
 
+        //Get the directions the player should move related
+        //to the direction the camera is currently facing
         Vector3 forwardVerticalInput = verticalInput * forward;
         Vector3 rightHorizontalInput = horizontalInput * right;
 
+        //Store the movement output
         moveOutput = forwardVerticalInput + rightHorizontalInput;
+
+        if (context.performed)
+        {
+            transform.rotation = Quaternion.LookRotation(moveOutput);
+        }
     }
+    //When method is called after pressing the button associated
+    //with the method in the input controller component of player
     public void OnJump(InputAction.CallbackContext context)
     {
+        //if the player is in the ground and button was pressed
         if(context.performed && controller.isGrounded)
         {
+            //modify velocity y vector to simulate a jump
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
     }
 
     private void Update()
     {
-        //Vertical&horizontal movement
+        //Makes the player move in the designated direction
+        //relative to the camera's direction and the rate of
+        //movement every second
         controller.Move(moveOutput * speed * Time.deltaTime);
 
-        //Jumping
+        //Reduces the player's velocity to simulate a fall
         velocity.y += gravity * Time.deltaTime;
+
+        //Modifies the player's vertical velocity
+        //according to changes made to it
         controller.Move(velocity * Time.deltaTime);
     }
 }
